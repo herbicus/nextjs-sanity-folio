1316354301b583a90666069669242e6a147067d3
Herb Torres <herbtorres960@gmail.comm>
2025-11-18 14:57:20 -0500

    removes headers config from next config

--- next.config.ts
+++ next.config.ts
@@ -9,19 +9,19 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  async headers() {
-    return [
-      {
-        source: "/studio/static/:path*",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=0, must-revalidate",
-          },
-        ],
-      },
-    ];
-  },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: "/studio/static/:path*",
+  //       headers: [
+  //         {
+  //           key: "Cache-Control",
+  //           value: "public, max-age=0, must-revalidate",
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
